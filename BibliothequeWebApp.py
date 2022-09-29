c412a2bdeabb8783428d3203a40bb77d845ebe96
formation1902 <formation1902@gmail.com>
2022-09-29 13:43:52 +0200

    initialisation p9 webapp

---
+++ BibliothequeWebApp.py
@@ -0,0 +1,39 @@
+
+#!/usr/bin/python
+from flask import Flask,render_template,url_for,request
+import requests
+import os
+from user import User
+
+
+
+def fp(*smth):
+    print('\n','#'*10)
+    print(smth)
+    print('#'*10,'\n')
+
+
+app = Flask(__name__,template_folder='templates')
+
+
+@app.route('/')
+def home():
+    fp('Je suis at home')
+    return render_template('welcome.html')
+
+
+
+@app.route('/sendUserInformation',methods=['POST'])
+def fx_any_user():
+    print("Gestion des données utilisateurs : ")
+    try:
+        current_user = User(request.form['user_id'])
+    except:
+        current_user = User(-1)
+    fp(current_user.toJson())
+    return "received"
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=33330)
